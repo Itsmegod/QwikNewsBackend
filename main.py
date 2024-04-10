d4c39a17afb44319872f8f795e6e552e030003bb
Itsmegod <166569713+Itsmegod@users.noreply.github.com>
2024-04-10 01:51:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -53,13 +53,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:5173"
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
